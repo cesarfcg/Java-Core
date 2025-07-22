4bfe696862852f5066344f20ee9db2542b7b329a
Fernando César <fernandocesar4141@gmail.com>
2025-07-22 19:31:47 -0300

    Herança 03 (Protected e construtores) | Java-core

--- src/academy/devdojo/maratonajava/Hheranca/Domain/Funcionario.java
+++ src/academy/devdojo/maratonajava/Hheranca/Domain/Funcionario.java
@@ -3,9 +3,16 @@ package academy.devdojo.maratonajava.Hheranca.Domain;
 public class Funcionario extends Pessoa{
     private double salario;
 
+    public Funcionario(String nome) {
+        super(nome);
+    }
+
     public void imprime(){
         super.imprime();
         System.out.println(this.salario);
+    }
+    public void relatorioSalarial(){
+        System.out.println(this.nome+" recebeu esse mês: R$"+this.salario);
     }
     public double getSalario() {
         return salario;
--- src/academy/devdojo/maratonajava/Hheranca/Test/HerancaTest01.java
+++ src/academy/devdojo/maratonajava/Hheranca/Test/HerancaTest01.java
@@ -7,20 +7,19 @@ import academy.devdojo.maratonajava.Hheranca.Domain.Pessoa;
 public class HerancaTest01 {
     public static void main(String[] args) {
         Endereco endereco = new Endereco();
-        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa("José");
         endereco.setCep(5946000);
         endereco.setRua("Rua Oscar Marinho");
         pessoa.setCpf("709769464554");
         pessoa.setNome("Lucas");
         pessoa.setEndereco(endereco);
-        Funcionario funcionario = new Funcionario();
+        Funcionario funcionario = new Funcionario("José");
         funcionario.setSalario(2600);
         funcionario.setNome("César");
         funcionario.setCpf("70858667589");
         funcionario.setEndereco(endereco);
         funcionario.imprime();
-
-
+        funcionario.relatorioSalarial();
 
     }
 }
